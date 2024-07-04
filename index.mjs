0c89dccddf19abc21be0de0808bee71dbef2a409
shiba10222 <thilder10222@gmail.com>
2024-07-04 14:13:52 +0800

    修改

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import http from "http";
 
 const server = http.createServer((request, response)=> {
     response.setHeader("content-type", "text/html;charset=utf-8")
-    response.end("Hello server, 你好主機", `${process.argv[2]},${process.argv[3]}`)
+    response.end(`Hello server, 你好主機", ${process.argv[2]},${process.argv[3]}`)
 
 });
 
